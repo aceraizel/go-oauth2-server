6f19702223124ceb2536ae09187bb2830f3cbd9f
thanhvuuit <thanhvu.uit@gmail.com>
2019-10-02 23:42:04 +0700

    initialize

---
+++ database/database_test.go
@@ -0,0 +1,23 @@
+package database_test
+
+import (
+	"errors"
+	"testing"
+
+	"../config"
+	"../database"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewDatabaseTypeNotSupported(t *testing.T) {
+	cnf := &config.Config{
+		Database: config.DatabaseConfig{
+			Type: "bogus",
+		},
+	}
+	_, err := database.NewDatabase(cnf)
+
+	if assert.NotNil(t, err) {
+		assert.Equal(t, errors.New("Database type bogus not suppported"), err)
+	}
+}
